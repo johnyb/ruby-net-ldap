f5404bfd50c53cb3d6656a9e3dd3928bbc182c79
Julian Bäume <julian@svg4all.de>
2013-04-11 21:52:12 +0200

    implement request value for RFC3062
    it’s now possible to create a request to change the password

--- lib/net/ldap/password.rb
+++ lib/net/ldap/password.rb
@@ -4,6 +4,15 @@ require 'digest/md5'
 require 'base64'
 
 class Net::LDAP::Password
+
+  PasswdModifyOID = "1.3.6.1.4.1.4203.1.11.1"
+
+  def initialize(args = {})
+    @user_id = args[:dn]
+    @old_password = args[:old_password]
+    @new_password = args[:new_password]
+  end
+
   class << self
     # Generate a password-hash suitable for inclusion in an LDAP attribute.
     # Pass a hash type as a symbol (:md5, :sha, :ssha) and a plaintext
@@ -33,5 +42,21 @@ class Net::LDAP::Password
          end
       return attribute_value
     end
+
   end
+
+  def to_ber
+    request = [ PasswdModifyOID.to_ber_contextspecific(2) ]
+    request << payload.to_ber_sequence unless payload.empty?
+    request.to_ber_appsequence(Net::LDAP::PDU::ExtendedRequest)
+  end
+
+  private
+    def payload
+      payload = []
+      payload << @user_id.to_ber if @user_id
+      payload << @old_password.to_ber if @old_password
+      payload << @new_password.to_ber if @new_password
+      return payload
+    end
 end
